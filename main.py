64ef63ba72b4858fb5e4d147e26c4276b516de5c
Akerke87 <akerke8786@gmail.com>
2019-03-02 12:03:41 +0500

    rectangle

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-from myclass import Tort
+class rectangle:
+    def __init__(self):
+        self.width=100
+        self.height=100
+        self.color='red'
+    def square (self):
+        return self.width*self.height
+    def perimeter(self):
+        return (self.width+self.height)*2
+    def showColor(self):
+        return (self.color)
+        
+
---
+++ myclass.py
@@ -0,0 +1,6 @@
+from main import rectangle
+
+rect = rectangle()
+print (rect.square())
+print(rect.perimeter())
+print (rect.showColor())
